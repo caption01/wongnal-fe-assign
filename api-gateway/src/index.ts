731a68f72833f6bb6a451dfbb5e61e3e93e35d85
nutchapon.h <nutchapon@enres.com>
2020-11-07 13:20:33 +0700

    [6] fix start up server text

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 
 const start = () => {
   app.listen(3000, () => {
-    console.log("tickets run on port 3000 !!");
+    console.log("server run on port 3000 !!");
   });
 };
 
