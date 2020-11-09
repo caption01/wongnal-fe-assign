5b109951114b055ea0b5f0c6d4376659b0977914
nutchapon.h <nutchapon@enres.com>
2020-11-09 21:12:25 +0700

    add env-web-example

--- web/src/axios/index.ts
+++ web/src/axios/index.ts
@@ -1,5 +1,5 @@
 import Axios, { AxiosInstance } from "axios";
 
 export const axios: AxiosInstance = Axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_APP_API_VERSION || "http://localhost:4000",
 });
